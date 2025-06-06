2fe2456d030da9822430c193363a4ecb48cbd91c
Abyss-Seeker <jasperSHSID@gmail.com>
2025-06-06 23:35:09 +0800

    Fixed event action not found error

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import re
 import aiohttp
 import asyncio
-from astrbot.api.event import filter, AstrMessageEvent
+from astrbot.api.event import filter, AstrMessageEvent, EventAction
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
